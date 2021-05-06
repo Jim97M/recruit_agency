d3a2e68d57e1d6a524bf6becdf45c06ec3f74bd9
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:06:38 +0300

    Services

---
+++ services.php
@@ -0,0 +1,128 @@
+<?php include 'includes/header.php' ?>
+
+
+<div id="indicator" class="w-100">
+    <h2>What we offer to our candidates and also our clients</h2>
+    <p>Choose to trust us and we create opportunities fine-tuned and tailored for specific demands as well as assess human resource amptitude in the market, our experience in this field help us to connect different employers and employees with particular needs to help conform to different scopes of needs in the market. Choose to trust us and we create opportunities fine-tuned and tailored for specific demands as well as assess human resource amptitude in the market, our experience in this field help us to connect different employers and employees with particular needs to help conform to different scopes of needs in the market</p>
+</div>
+
+
+<div id="icons" class="row">
+<a href="">
+  <lord-icon
+      src="https://cdn.lordicon.com//dxjqoygy.json"
+      trigger="loop"
+      colors="primary:#121331,secondary:#08a88a"
+      stroke="70"
+      style="width:150px;height:150px">
+  </lord-icon>
+  <p style="font-size:20px; color:black; font-weight: 500">Connect</p>
+</a>
+<a href="">
+  <lord-icon
+    src="https://cdn.lordicon.com//msoeawqm.json"
+    trigger="loop"
+    colors="primary:#121331,secondary:#08a88a"
+    style="width:150px;height:150px">
+  </lord-icon>
+  <p style="font-size:20px; color:black; font-weight: 500">Find</p>
+</a>
+<a href="">
+   <lord-icon
+      src="https://cdn.lordicon.com//zzcjjxew.json"
+      trigger="loop"
+      colors="primary:#faddd1,secondary:#a5e830"
+      style="width:150px;height:150px">
+   </lord-icon>
+   <p style="font-size:20px; color:black; font-weight: 500">Locate</p>
+</a>
+<a href="">
+  <lord-icon
+      src="https://cdn.lordicon.com//gqdnbnwt.json"
+      trigger="loop"
+      colors="primary:#121331,secondary:#f4b69c"
+      style="width:150px;height:150px">
+  </lord-icon>
+  <p style="font-size:20px; color:black; font-weight: 500">Compare</p>
+</a>
+</div>
+
+	<!-- what we do section -->
+  <div class="what bg-li py-5" id="what">
+			<div class="container py-xl-5 py-lg-3">
+				<div class="row about-bottom-w3l text-center mt-4">
+					<div class="col-lg-4 about-grid">
+						<div class="about-grid-main">
+							<img style="width:100px; height:100px;"
+								src="https://res.cloudinary.com/dqpsoxe38/image/upload/v1619508182/EBC/profile_lhuv7u.png"
+								alt="" class="img-fluid">
+							<h4 class="my-4">Connect</h4>
+							<!-- <button class="accordion">America</button> -->
+							<div class="panel">
+								<p>Work In America, both north and south America to establish about a string of
+									experience</p>
+							</div>
+							<!-- <button class="accordion">Europe</button> -->
+							<div class="panel">
+								<p>Get to know the European countriens with linient visa requirements and best working
+									environments</p>
+							</div>
+
+							<!-- <button class="accordion">Asia</button> -->
+							<div class="panel">
+								<p>Middle East Opportunities available with employers ready to
+									assimilate Qickly</p>
+							</div>
+
+						</div>
+					</div>
+					<div class="col-lg-4 about-grid my-lg-0 my-5">
+						<div class="about-grid-main">
+							<img style="width:100px; height:100px;"
+								src="https://res.cloudinary.com/dqpsoxe38/image/upload/v1619508163/EBC/profile1_nyo9dj.png"
+								alt="" class="img-fluid">
+							<h4 class="my-4">Find</h4>
+							<!-- <button class="accordion">Health</button> -->
+							<div class="panel">
+								<p>Obtain necessary documentation as stipulated by job description, if any difficulty
+									contact us</p>
+							</div>
+							<!-- <button class="accordion">Security</button> -->
+							<div class="panel">
+								<p>Visit necessary institutions for security clearance and conform to requirements per
+									country</p>
+							</div>
+
+							<!-- <button class="accordion">Visa</button> -->
+							<div class="panel">
+								<p>Apply Visa and necessary travelling and working documentation</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 about-grid">
+						<div class="about-grid-main">
+							<img style="width:100px; height:100px;"
+								src="https://res.cloudinary.com/dqpsoxe38/image/upload/v1619507895/EBC/profile4_tpglnw.png"
+								alt="" class="img-fluid">
+							<h4 class="my-4">Compare</h4>
+							<!-- <button class="accordion">Integration</button> -->
+							<div class="panel">
+								<p>Key to integration is communication, make sure you understand language in job
+									description</p>
+							</div>
+							<!-- <button class="accordion"> Country Policy</button> -->
+							<div class="panel">
+								<p> Understand Regional and specific country rules, regulations, policies and cultural perceptions </p>
+							</div>
+
+							<!-- <button class="accordion">Ethics</button> -->
+							<div class="panel">
+								<p>Be willing to do your jobs diligently to more than satisfactory levels</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- //what we do section -->
+<?php  include 'footer.php' ?>
