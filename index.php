4f77f3e3278c8a915aaa2f0964fb46ee852ea23a
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:04:53 +0300

    HomePage

---
+++ index.php
@@ -0,0 +1,65 @@
+ <?php  include './includes/header.php'; ?>
+
+
+    <div class="home" style="overflow-y:hidden" >
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <!-- <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol> -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="./images/jhome.jpg" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>
+                        Our Commitment</h5>
+                    <p>
+                        Help out those who wan't to be helped</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./images/jhome2.jpg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>
+                        Our Interest</h5>
+                    <p>
+                        Having Broader Scope In regard to Labor Specifications</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./images/jhome3.jpg" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>
+                        Impact</h5>
+                    <p>
+                        Both Economical and Social additives have culminated into positive results
+                    </p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <!-- <span class="sr-only">Previous</span> -->
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <!-- <span class="sr-only">Next</span> -->
+        </a>
+
+
+    </div>
+
+      <div class="section">
+        <div class="container">
+            <h2>Recruit Agency Offering Guidance Through Human Resource Empowerment</h2>
+            <p>Agency Offering quality human resource guidance through prolific assessment of international job markets
+                and communication with labor standard organizations and unions</p>
+        </div>
+
+        <!-- <div class="infogram-embed" data-id="0fd0f772-75aa-4180-aec5-a93ab315acd1" data-type="interactive" data-title="Untitled chart"></div><script>!function(e,i,n,s){var t="InfogramEmbeds",d=e.getElementsByTagName("script")[0];if(window[t]&&window[t].initialized)window[t].process&&window[t].process();else if(!e.getElementById(n)){var o=e.createElement("script");o.async=1,o.id=n,o.src="https://e.infogram.com/js/dist/embed-loader-min.js",d.parentNode.insertBefore(o,d)}}(document,0,"infogram-async");</script><div style="padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px"><a href="https://infogram.com/0fd0f772-75aa-4180-aec5-a93ab315acd1" style="color:#989898!important;text-decoration:none!important;" target="_blank">Untitled chart</a><br><a href="https://infogram.com" style="color:#989898!important;text-decoration:none!important;" target="_blank" rel="nofollow">Infogram</a></div> -->
+       </div>
+    </div>
+    <?php include 'footer.php'  ?>
+
+
