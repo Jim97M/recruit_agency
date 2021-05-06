a9abc88ecd89a28f38eff1fe443b81f74b782c65
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:10:28 +0300

    Includes

---
+++ candidates.php
@@ -0,0 +1,106 @@
+  <?php include 'includes/header.php' ?>
+
+ <div id="indicator" class="w-100">
+        <h2>What we offer to our candidates and also our clients</h2> 
+     <div id="icons" class="row">
+
+
+ <a href="logreg/login.php">
+   
+     <lord-icon
+       src="https://cdn.lordicon.com//dxjqoygy.json"
+       trigger="loop"
+       colors="primary:#121331,secondary:#08a88a"
+       stroke="14"
+       style="width:150px;height:150px">
+     </lord-icon>
+     <p style="font-weight:400; color:black; font-size: 19px" class="icons">Account</p>
+     </a>
+  <a href="">   
+     <lord-icon
+       src="https://cdn.lordicon.com//nocovwne.json"
+       trigger="loop"
+       colors="primary:#121331,secondary:#08a88a"
+       stroke="10"
+       style="width:150px;height:150px">
+     </lord-icon>
+    <p style="font-weight:400; color:black; font-size: 19px">Try Out</p>
+     </a>
+   <a href="">
+     <lord-icon
+       src="https://cdn.lordicon.com//msoeawqm.json"
+       trigger="loop"
+       colors="primary:#121331,secondary:#08a88a"
+       stroke="15"
+       style="width:150px;height:150px">
+     </lord-icon>
+    </a>
+  <a href="">
+     <lord-icon
+        src="https://cdn.lordicon.com//gqzfzudq.json"
+        trigger="loop"
+        colors="primary:#121331,secondary:#08a88a"
+        stroke="12"
+        style="width:150px;height:150px">
+     </lord-icon>
+     <p style="font-weight:400; color:black; font-size: 19px">Region</p>
+     </a>
+  <a href="">
+     <lord-icon
+       src="https://cdn.lordicon.com//jqeuwnmb.json"
+       trigger="loop"
+       colors="primary:#121331,secondary:#08a88a"
+       stroke="16"
+       style="width:150px;height:150px">
+     </lord-icon>
+     <p style="font-weight:400; color:black; font-size: 19px">Read</p>
+     </a>
+   </div>
+
+   <!-- Below Sec -->
+
+</div>
+
+
+<div class="row w-100 h-40">
+  <div class="col-sm-8 mt-2">
+      <p>Looking for opportunity? We listen we evaluate and we try to care about our clients and other partners problem, we offer the best outlook into the labor industry as far as human resource is concerned</p>
+  </div>
+  <div class="col-sm-4 mt-1 mb-1">
+  <div class="card text-center">
+    <div class="card-header">Featured</div>
+     <div class="card-body">
+      <h5 class="card-title mr-0">Type Worker</h5>
+       <a href="apply.php" class="btn btn-primary ml-0">Apply</a>
+     </div>
+    <div class=" text-muted">2 days ago</div>
+   </div>
+   <div class="card text-center mt-1">
+     <div class="card-body">
+      <h5 class="card-title mr-0">Site Manager</h5>
+       <a href="#" class="btn btn-primary ml-0">Apply</a>
+     </div>
+    <div class=" text-muted">4 days ago</div>
+   </div>
+
+   <div class="wrapper mt-5 mb-0">
+
+   <a href="careers.php">
+  <button class="button">
+    View All
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  </a>
+</div>
+
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+  <?php include 'footer.php' ?>
---
+++ includes/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="property" charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+    <script src="https://cdnjs.com/libraries/bodymovin" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="images/logo.png">
+    <script src="../javascript/main.js">  </script>
+</head>
+
+<body>
+    <nav style="background-color: azure;" class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Agency</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.php">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="careers.php">Careers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="candidates.php">Candidates</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    
+                    <!-- <div class="row" style="float: right; margin-right:0">
+                
+                        <i class="fab fa-facebook-f fa-1x "></i>
+                    
+                        <i class="fab fa-twitter fa-1x " style="float:right"></i>
+                
+                    </div> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
