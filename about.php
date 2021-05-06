52ccac3986941f02c88cd903fa83c949ab9ddfb5
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:05:35 +0300

    About

---
+++ about.php
@@ -0,0 +1,69 @@
+<?php include 'includes/header.php'  ?>
+
+<div class="jumbotron text-center">
+        <h1>Learn More About Who We are</h1>
+        <p>Why You Should Get To Know Us and Why You Should Trust and Choose Us</p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Our History</h3>
+                <p>We are an established agency seeking to provide quality guidance in the labor industry</p>
+                <p>founded in 2021, this agency has gained trust among its clients and attracts more businesses and
+                    employers</p>
+            </div>
+
+            <div class="col-sm-4">
+                <h3>Our Activities</h3>
+                <p>We keep contact with employers and seek many approvals from regulatory bodies to have employers look
+                    for human resources through Us</p>
+                <p>Look for qualified human resource persons among the public and have much more trusted employees
+                    connected to employers with high ethical standards</p>
+            </div>
+
+            <div class="col-sm-4">
+                <h3>Our Priorities</h3>
+                <p>We keep contact with employers and seek many approvals from regulatory bodies to have employers look
+                    for human resources through Us</p>
+                <p>Look for qualified human resource persons among the public and have much more trusted employees
+                    connected to employers with high ethical standards</p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+
+      <div id="fsection" class="col-sm-4 ml-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-layer-backward text-success" viewBox="0 0 16 16">
+         <path d="M8.354 15.854a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 0-.708l1-1a.5.5 0 0 1 .708 0l.646.647V4H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9v7.793l.646-.647a.5.5 0 0 1 .708 0l1 1a.5.5 0 0 1 0 .708l-3 3z"/>  
+         <path d="M1 9a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h4.5a.5.5 0 0 1 0 1H1v2h4.5a.5.5 0 0 1 0 1H1zm9.5 0a.5.5 0 0 1 0-1H15V6h-4.5a.5.5 0 0 1 0-1H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4.5z"/>
+        </svg>
+    
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-stickies-fill" viewBox="0 0 16 16">
+         <path d="M0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1H1.5A1.5 1.5 0 0 0 0 1.5z"/>
+         <path d="M3.5 2A1.5 1.5 0 0 0 2 3.5v11A1.5 1.5 0 0 0 3.5 16h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 16 9.586V3.5A1.5 1.5 0 0 0 14.5 2h-11zm6 8.5a1 1 0 0 1 1-1h4.396a.25.25 0 0 1 .177.427l-5.146 5.146a.25.25 0 0 1-.427-.177V10.5z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
+         <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+         <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+        </svg>
+      </div>
+  
+     <div class="col-sm-4">
+       <img src="images/noun_person_13474.png" width="192" height="120" />
+       <img src="images/noun_person_5162.png" width="192" height="120" />
+     </div>
+
+ 
+</div>
+  <div class="row mt-3" style="background-color: grey; width: 100%;">
+    <div class="col-sm-4">
+    <!-- <span>agency@mail.com</span> -->
+       <img src="images/africa.jpg" style="width: 100px; height: 100px; margin-left:174vh" alt="">
+    </div>
+  </div>
+   
+
+   <?php include 'footer.php' ?>
