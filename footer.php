681cfe30aa7e87454cbc6cf8d3b21168f9fb8f8c
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:09:07 +0300

    Footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="fcontainer mb-0">
+  <div class="footer mb-0">
+    <footer>
+       <p><span>&copy</span>CopyRight Reserved @ agency 2021</p>
+    </footer>
+  </div>
+</div>
+
+  </body>
+
+</html>
