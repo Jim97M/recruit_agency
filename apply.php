922ef1d59e4a57c473aa2991354b7928b3007cdd
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:07:42 +0300

    Apply

---
+++ apply.php
@@ -0,0 +1,70 @@
+
+<?php include 'includes/header.php' ?>
+
+<div id="indicator" class="w-100">
+    <h2>Finding Expertise</h2>
+    <i class="fas fa-search fa-5x"></i>
+</div>
+
+
+<div class="container">
+
+<form action="">
+
+<div class="row" id="inputC">
+
+  <div  class="col-sm-4">
+   
+   <label id="label" for="firstname">First Name <span>&#42;</span></label>
+  <input type="text">
+   </div>
+
+  <div class="col-sm-4">
+   <label id="label" for="lastname">Last Name <span>&#42;</span></label>
+   <input type="text">
+    </div>
+    <div  class="col-sm-4">
+   
+   <label id="label" for="firstname">First Name <span>&#42;</span></label>
+  <input type="text">
+   </div>
+
+  <div class="col-sm-4">
+   <label id="label" for="lastname">Last Name <span>&#42;</span></label>
+   <input type="text">
+    </div>
+    <div  class="col-sm-4">
+   
+   <label id="label" for="firstname">First Name <span>&#42;</span></label>
+  <input type="text">
+   </div>
+
+  <div class="col-sm-4">
+   <label id="label" for="lastname">Last Name <span>&#42;</span></label>
+   <input type="text">
+    </div>
+    <div  class="col-sm-4">
+   
+   <label id="label" for="firstname">First Name <span>&#42;</span></label>
+  <input type="text">
+   </div>
+
+  <div class="col-sm-4">
+   <label id="label" for="lastname">Last Name <span>&#42;</span></label>
+   <input type="text">
+    </div>
+
+    <div  class="col-sm-4">
+   <label id="label" for="firstname">First Name <span>&#42;</span></label>
+  <input type="text">
+   </div>
+
+  <div class="col-sm-4">
+   <label id="label" for="lastname">Last Name <span>&#42;</span></label>
+   <input type="text">
+    </div>
+
+</div>
+ </form>
+
+</div>
