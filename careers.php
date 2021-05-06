e44b99c3919683ac623564e6b8f700e5044a6218
Jim97M <wabuyajames@gmail.com>
2021-05-06 09:08:26 +0300

    Careers

---
+++ careers.php
@@ -0,0 +1,216 @@
+<?php include 'includes/header.php'?>
+
+<div class="container">
+    <form action=""method="POST">
+       <div class="row" id="inputB">
+          <div class="col-sm-4">
+             <label for="Job Title">Job Title</label>
+                <input type="text"name="title"required> 
+          </div>
+
+         <div class="col-sm-4">
+         <label for="Region">Region</label>
+               <input type="text"name="title"required>
+         </div>
+
+         <div class="col-sm-4">
+         <label for="Job Title">Select</label>
+              <input type="text"name="title"required>
+         </div>
+      </div>
+   </form>
+     <div class="row mt-5">
+      
+       
+          <div class="col-sm-4"> <h2>Latest Posted</h2></div>
+      
+         <div class="col-sm-4"><h2>Region</h2></div>
+       
+        <div class="col-sm-4"><h2>More Info</h2></div>
+  </div>
+  <hr>   
+
+  <div class="row mt-4">
+      
+      <div  class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Construction Worker</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> Middle East</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Abu Dhabi Based Construction Company Looking For Labor In Storeys and Installing Various Inner Components of the building</p>
+
+        </div>
+    
+</div>
+
+<hr>
+
+
+<div class="row mt-4">
+      <div class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Site Manager</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> Middle East</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Abu Dhabi Based Construction Company Looking For Labor In Storeys and Installing Various Inner Components of the building</p>
+
+        </div>
+
+</div>
+
+<hr>
+
+<div class="row mt-4">
+      <div class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Digital Marketter</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> Europe</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Geneva Based Company Specializing In Wear and Wrist watches and has subsidiaries through out western Europe</p>
+
+        </div>
+
+</div>
+
+<hr>
+
+<div class="row mt-4">
+      <div class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Product Overseer</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> Africa</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Geneva Based Company Specializing In Wear and Wrist watches and has subsidiaries through out western Europe</p>
+
+        </div>
+
+</div>
+
+<hr>
+
+<div class="row mt-4">
+      <div class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Plant Operator</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> South America</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Geneva Based Company Specializing In Wear and Wrist watches and has subsidiaries through out western Europe</p>
+
+        </div>
+
+</div>
+
+<hr>
+
+<div class="row mt-4">
+      <div class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Digital Marketter</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> North America</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Geneva Based Company Specializing In Wear and Wrist watches and has subsidiaries through out western Europe</p>
+
+        </div>
+
+</div>
+
+<hr>
+
+<div class="row mt-4">
+      <div class="col-sm-4 mt-3">
+         <a href="cnworker.php"> <h4>Digital Marketter</h4> </a>
+
+        </div>
+
+        
+        <div class="col-sm-4 mt-3">
+        
+         <a href=""> <h4> Europe</h4> </a>
+
+        </div>
+
+
+        <div class="col-sm-4 mt-3">
+        
+          <p>Geneva Based Company Specializing In Wear and Wrist watches and has subsidiaries through out western Europe</p>
+
+        </div>
+
+</div>
+   </div>
+  <div class="row  ml-0">
+     <ul class="pagination">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">4</a></li>
+      <li class="page-item active"><a class="page-link" href="#">5</a></li>
+      <li class="page-item"><a class="page-link" href="#">6</a></li>
+      <li class="page-item"><a class="page-link" href="#">7</a></li>
+      <li class="page-item"><a class="page-link" href="#">8</a></li>
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+     </ul>
+  </div>
+
+  <?php include 'footer.php'?>
